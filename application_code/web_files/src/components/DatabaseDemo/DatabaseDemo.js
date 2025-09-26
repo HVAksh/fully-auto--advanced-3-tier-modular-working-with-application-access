553c863b6a2e46c7ba96ab7f3b47bd086edfb00c
root <root@SS.localdomain>
2025-09-26 17:35:13 +0530

    db and package update in webb

--- application_code/web_files/src/components/DatabaseDemo/DatabaseDemo.js
+++ application_code/web_files/src/components/DatabaseDemo/DatabaseDemo.js
@@ -39,7 +39,7 @@
                 await new Promise(resolve => setTimeout(resolve, 1000)); 
                 return await this.fetch_retry(url, n - 1);
             }
-        };
+        }
 
 
           renderTableData() {
