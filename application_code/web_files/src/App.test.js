200ec67f095ef202d370069bd8536e5fb481eac6
HVAksh <chaudharyaksh92@gmail.com>
2025-09-26 19:20:31 +0530

    Update App.test.js

--- application_code/web_files/src/App.test.js
+++ application_code/web_files/src/App.test.js
@@ -1,13 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders App component with navigation', () => {
-  render(
-    <HashRouter>
-      <App />
-    </HashRouter>
-  );
+  render(<App />);
   const burgerElement = screen.getByLabelText(/Toggle menu/i);
   expect(burgerElement).toBeInTheDocument();
 });
